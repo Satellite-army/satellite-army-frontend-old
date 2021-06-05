0048c5152ffdcff7cb37324439aa30212ea1dd66
Lorenzo Alluminio <alluminio.lorenzo@gmail.com>
2021-06-05 23:57:32 +0200

    farming page

--- src/views/farming/index.tsx
+++ src/views/farming/index.tsx
@@ -1,30 +1,50 @@
-import { Card } from "antd";
+import { Button, Card, Col, InputNumber, Popover, Radio, Row, Space } from "antd";
 import React from "react";
+import { TokenIcon } from "../../components/TokenIcon";
 import { LABELS } from "../../constants";
 import { useLendingReserves } from "../../hooks";
-import { BorrowItem } from "./item";
 import "./itemStyle.less";
+import { SendOutlined, BellFilled } from "@ant-design/icons";
+import { FarmingItem } from "./item";
+
 
 export const FarmingView = () => {
-  const { reserveAccounts } = useLendingReserves();
+  // bell popup content
+
+  const pools = [{
+    liquidityLink: "https://raydium.io/liquidity/?ammId=58oQChx4yWmvKdwLLZzBi4ChoCc2fqCUWBkwMihLYQo2",
+    lpTokenValue: 1.08,
+    mint1: "So11111111111111111111111111111111111111112",
+    mint2: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+    token1: "SOL",
+    token2: "USDC",
+    tvl: "11,546,870.35",
+    lpNumber: 46.4,
+    lpTotalValue: 50,
+    distribution1: 80,
+    distribution2: 0.5,
+    last: "03/06/2021",
+    yieldToken: 140,
+    yieldDollar: 150,
+    ILPercentage: 3,
+    ILDollar: 300,
+    fee: 0.5,
+    optimal: 1,
+    zero1: 40,
+    zero2: 1,
+    firstDeposit: "01/05/2021",
+    harvestCompoundTimes: 3,
+    avgCompound: 5,
+    totalFees: 500
+  }]
+
+
   return (
     <div className="flexColumn">
-      <Card>
-        <div className="borrow-item deposit-header">
-          <div>{LABELS.TABLE_TITLE_ASSET}</div>
-          <div>Serum Dex Price</div>
-          <div>{LABELS.TABLE_TITLE_MAX_BORROW}</div>
-          <div>{LABELS.TABLE_TITLE_APY}</div>
-          <div></div>
-        </div>
-        {reserveAccounts.map((account) => (
-          <BorrowItem
-            key={account.pubkey.toBase58()}
-            reserve={account.info}
-            address={account.pubkey}
-          />
-        ))}
-      </Card>
+       {pools.map((pool) => (
+         <FarmingItem props={pool} />
+       ))}
+      
     </div>
   );
 };
--- src/views/farming/item.tsx
+++ src/views/farming/item.tsx
@@ -3,48 +3,94 @@ import { useTokenName, useBorrowingPower } from "../../hooks";
 import { calculateBorrowAPY, LendingReserve } from "../../models/lending";
 import { TokenIcon } from "../../components/TokenIcon";
 import { formatNumber, formatPct } from "../../utils/utils";
-import { Button } from "antd";
+import { Button, Card, Col, InputNumber, Popover, Radio, Row, Space } from "antd";
 import { Link } from "react-router-dom";
 import { PublicKey } from "@solana/web3.js";
 import { LABELS } from "../../constants";
 import { useMidPriceInUSD } from "../../contexts/market";
+import { SendOutlined, BellFilled } from "@ant-design/icons";
 
-export const BorrowItem = (props: {
-  reserve: LendingReserve;
-  address: PublicKey;
-}) => {
-  const name = useTokenName(props.reserve.liquidityMint);
-  const price = useMidPriceInUSD(props.reserve.liquidityMint.toBase58()).price;
-
-  const { borrowingPower, totalInQuote } = useBorrowingPower(props.address);
-
-  const apr = calculateBorrowAPY(props.reserve);
-
-  return (
-    <Link to={`/borrow/${name}`}>
-      <div className="borrow-item">
-        <span style={{ display: "flex" }}>
-          <TokenIcon mintAddress={props.reserve.liquidityMint} />
-          {name}
-        </span>
-        <div>${formatNumber.format(price)}</div>
-        <div>
-          <div>
-            <div>
-              <em>{formatNumber.format(borrowingPower)}</em> {name}
-            </div>
-            <div className="dashboard-amount-quote">
-              ${formatNumber.format(totalInQuote)}
-            </div>
-          </div>
-        </div>
-        <div>{formatPct.format(apr)}</div>
-        <div>
-          <Button type="primary">
-            <span>{LABELS.BORROW_ACTION}</span>
-          </Button>
-        </div>
+export const FarmingItem = (props: any) => {
+  const tgAlertPop = (
+    <><div style={{ display: "flex" }}>
+      <div>
+        <Radio.Group>
+          <Space direction="vertical">
+            <Radio value={1}>{'>'}</Radio>
+            <Radio value={2}>=</Radio>
+            <Radio value={3}>{'<'}</Radio>
+          </Space>
+        </Radio.Group>
       </div>
-    </Link>
+      <div style={{ display: "flex", alignItems: "center", justifyContent: "center", maxWidth: "50%" }}>
+        <InputNumber style={{ verticalAlign: "middle" }} min={0} defaultValue={0} />
+      </div>
+    </div>
+      <div style={{ textAlign: "center" }}>
+        <Button
+          type="primary"
+          icon={<SendOutlined />} />
+      </div></>
+  );
+
+  const lpTokenPop = (
+    <p>1 LP = {props.props.lpTokenValue} $</p>
+  )
+
+  const securityPop = (
+    <div>
+      <div>
+        <p>Do you want to signup for security alerts related to this pool?</p>
+      </div>
+      <div style={{ textAlign: "center" }}>
+        <Button
+          type="primary"
+          icon={<SendOutlined />} />
+      </div>
+    </div>
+  )
+  return (
+    <Card style={{marginBottom:"10px"}}>
+      <div className="deposit-header" style={{ display: "flex" }}>
+        <div style={{ flexShrink: "unset" }}>
+          <Row>
+            <Col span="3"><TokenIcon style={{ marginLeft: 10, marginRight: 20 }} mintAddress={props.props.mint1} /><TokenIcon mintAddress={props.props.mint2} /></Col>
+            <Col span="4" style={{ textAlign: "left" }}>N LP Token / Value:</Col>
+            <Col span="4" style={{ textAlign: "left" }}>Yield</Col>
+            <Col span="4" style={{ textAlign: "left" }}>Impermanent Loss</Col>
+            <Col span="4" style={{ textAlign: "left" }}>Compounding</Col>
+            <Col span="4" style={{ textAlign: "left" }}>First deposit: {props.props.firstDeposit}</Col>
+          </Row>
+          <Row>
+            <Col span="3" style={{ textAlign: "left" }}>{props.props.token1} / {props.props.token2}</Col>
+            <Col span="4" style={{ textAlign: "left" }}><Popover content={lpTokenPop} title="Single LP token price" trigger="hover">{props.props.lpNumber} / {props.props.lpTotalValue} $</Popover></Col>
+            <Col span="4" style={{ textAlign: "left" }}>From last deposit / withdrawal</Col>
+            <Col span="4" style={{ textAlign: "left" }}>{props.props.ILPercentage}% / {props.props.ILDollar} $</Col>
+            <Col span="4" style={{ textAlign: "left" }}>Current fee: {props.props.fee} $</Col>
+            <Col span="4" style={{ textAlign: "left" }}>Harvested / Compounded {props.props.harvestCompoundTimes} times</Col>
+          </Row>
+          <Row>
+            <Col span="3" style={{ textAlign: "left" }}> TVL: $ {props.props.tvl} </Col>
+            <Col span="4" style={{ textAlign: "left" }}>Distribution:</Col>
+            <Col span="4" style={{ textAlign: "left" }}>{props.props.last}</Col>
+            <Col span="4" style={{ textAlign: "left" }}>0 if: {props.props.token1}={props.props.zero1} $ and {props.props.token2}={props.props.zero2}$</Col>
+            <Col span="4" style={{ textAlign: "left" }}>Optimal compounding time</Col>
+            <Col span="4" style={{ textAlign: "left" }}>Average compounding time: {props.props.avgCompound} days</Col>
+          </Row>
+          <Row>
+            <Col span="3" style={{ textAlign: "left" }}>
+              <a href={props.props.liquidityLink}>Add liquidity</a>
+            </Col>
+            <Col span="4" style={{ textAlign: "left" }}>{props.props.distribution1} / {props.props.distribution2}</Col>
+            <Col span="4" style={{ textAlign: "left" }}>{props.props.yieldToken} token / {props.props.yieldDollar} $</Col>
+            <Col span="4" style={{ textAlign: "left" }}>IL alert: <Popover content={tgAlertPop} title="Telegram Alert" trigger="click"><BellFilled style={{ cursor: "pointer", fontSize: "1.5em" }} /></Popover></Col>
+            <Col span="4" style={{ textAlign: "left" }}>{props.props.optimal} time a day</Col>
+            <Col span="4" style={{ textAlign: "left" }}>Total paid fees: {props.props.totalFees} $</Col>
+          </Row>
+        </div>
+        <div style={{ maxWidth: "50px", textAlign: "center", marginTop: "30px" }}><Popover content={securityPop} title="Telegram Alert" trigger="click"><BellFilled style={{ cursor: "pointer", fontSize: "2em" }} /></Popover></div>
+
+      </div>
+    </Card>
   );
 };
