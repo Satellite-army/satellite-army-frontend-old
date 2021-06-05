5e21e90c8fcb283ee001771117df514f381f8d7b
Marco <marco.brugali@runelab.it>
2021-06-05 17:13:44 +0200

    feat(lottie): add planet on onboarding

--- src/views/presentation/index.tsx
+++ src/views/presentation/index.tsx
@@ -13,6 +13,10 @@ import { BarChartStatistic } from "./../../components/BarChartStatistic";
 import "./itemStyle.less";
 import {Icon} from "../../assets";
 import {Link} from "react-router-dom";
+import Lottie from 'react-lottie';
+import planet1 from '../../assets/animation/planet1.json'
+import planet2 from '../../assets/animation/planet2.json'
+import planet3 from '../../assets/animation/planet3.json'
 
 export const PresentationPage = () => {
   const { reserveAccounts } = useLendingReserves();
@@ -21,29 +25,52 @@ export const PresentationPage = () => {
   const { tokenMap } = useConnectionConfig();
 
 
+  const renderAnimation = (animationData: any) => {
+    const defaultOptions = {
+      loop: true,
+      autoplay: true,
+      animationData: animationData,
+      rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+      }
+    };
+
+    return (
+      <Lottie options={defaultOptions}
+              height={150}
+              width={150}
+              isStopped={false}
+              isPaused={false}/>
+    )
+  }
+
   return (
     <div className="home-background">
       <Card className="header">
         <img className="header-icon" src={Icon}/>
       </Card>
+
       <div className="body">
         <div className="title" ><img src="/scritta.png" width="400" height="145"/></div>
         <div className="subtitle">The most complete tracking tool for DeFi, native to Solana</div>
 
         <div className="features">
           <div className="feature feature-top">
+            {renderAnimation(planet1)}
             <div className="circle">
               <div className="feature-title">Feature 1</div>
             </div>
             <div className="feature-description">lorem ipsum dolor sit amet</div>
           </div>
           <div className="feature">
+            {renderAnimation(planet2)}
             <div className="circle">
               <div className="feature-title">Feature 2</div>
             </div>
             <div className="feature-description">lorem ipsum dolor sit amet</div>
           </div>
           <div className="feature feature-top">
+            {renderAnimation(planet3)}
             <div className="circle">
               <div className="feature-title">Feature 3</div>
             </div>
