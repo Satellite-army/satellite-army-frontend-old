af662bf13714c86c1fc349dab288c51b9c2cc17c
Marco <marco.brugali@runelab.it>
2021-06-06 15:05:57 +0200

    Updates

---
+++ precache-manifest.ca5cb56c95c716191269c3bedf982bf4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "265b6d88110ef5de3657ae7403a3af05",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9447c0a4c6c80ddc5ae9",
+    "url": "./static/css/2.d1dfd3d6.chunk.css"
+  },
+  {
+    "revision": "ba81c0c7b32e47ffa5eb",
+    "url": "./static/css/main.40b81663.chunk.css"
+  },
+  {
+    "revision": "9447c0a4c6c80ddc5ae9",
+    "url": "./static/js/2.7ba2ef2c.chunk.js"
+  },
+  {
+    "revision": "d60dc7c30d149ea169748a476fc1240f",
+    "url": "./static/js/2.7ba2ef2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba81c0c7b32e47ffa5eb",
+    "url": "./static/js/main.9395909a.chunk.js"
+  },
+  {
+    "revision": "f4270ecf90b56311f2d1",
+    "url": "./static/js/runtime-main.0ad816d2.js"
+  },
+  {
+    "revision": "9679baf1bd1251d06afd43bba3ef5d57",
+    "url": "./static/media/discordIcon.9679baf1.png"
+  },
+  {
+    "revision": "d5fd752f7fae87fc7700f9362a7ce936",
+    "url": "./static/media/homeBackground.d5fd752f.jpg"
+  },
+  {
+    "revision": "b3b0f9c4f88a66fd531979804fbfcd22",
+    "url": "./static/media/icon.b3b0f9c4.png"
+  },
+  {
+    "revision": "1c9e553b6ef2d38e86913a9a62ee5c2d",
+    "url": "./static/media/ledger.1c9e553b.svg"
+  },
+  {
+    "revision": "0b4a504aad27ed13dc45215aa77e505c",
+    "url": "./static/media/mathwallet.0b4a504a.svg"
+  },
+  {
+    "revision": "26e1b2402e670b81d8c1d898a166e5dc",
+    "url": "./static/media/solflare.26e1b240.svg"
+  },
+  {
+    "revision": "572321278f7b43bf077c53cc0655b99a",
+    "url": "./static/media/sollet.57232127.svg"
+  },
+  {
+    "revision": "64c4ae6977aacf1a0547467b2a5fc9cd",
+    "url": "./static/media/telegramIcon.64c4ae69.png"
+  },
+  {
+    "revision": "3b32a34b447c5c3554faabff66568bba",
+    "url": "./static/media/viola.3b32a34b.jpg"
+  }
+]);
