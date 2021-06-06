d929bc52ba01499ef314b38e84cd168240ad77b1
Marco <marco.brugali@runelab.it>
2021-06-06 18:41:09 +0200

    feat(farms): add farm and color

--- src/views/farming/item.tsx
+++ src/views/farming/item.tsx
@@ -54,37 +54,37 @@ export const FarmingItem = (props: any) => {
       <div className="deposit-header" style={{ display: "flex" }}>
         <div style={{ flexShrink: "unset" }}>
           <Row>
-            <Col span="3"><TokenIcon style={{ marginLeft: 10, marginRight: 20 }} mintAddress={props.props.mint1} /><TokenIcon mintAddress={props.props.mint2} /></Col>
+            <Col span="3" style={{ display: "flex" }}><TokenIcon style={{ marginLeft: 10, marginRight: 20 }} mintAddress={props.props.mint1} /><TokenIcon mintAddress={props.props.mint2} /></Col>
             <Col span="4" style={{ textAlign: "left" }}>N LP Token / Value:</Col>
             <Col span="4" style={{ textAlign: "left" }}>Yield</Col>
             <Col span="4" style={{ textAlign: "left" }}>Impermanent Loss</Col>
             <Col span="4" style={{ textAlign: "left" }}>Compounding</Col>
-            <Col span="4" style={{ textAlign: "left" }}>First deposit: {props.props.firstDeposit}</Col>
+            <Col span="4" style={{ textAlign: "left", color: "rgb(242,193,51)" }}>First deposit: {props.props.firstDeposit}</Col>
           </Row>
           <Row>
-            <Col span="3" style={{ textAlign: "left" }}>{props.props.token1} / {props.props.token2}</Col>
-            <Col span="4" style={{ textAlign: "left" }}><Popover content={lpTokenPop} title="Single LP token price" trigger="hover">{props.props.lpNumber} / {props.props.lpTotalValue} $</Popover></Col>
+            <Col span="3" style={{ textAlign: "left", color: "rgb(242,193,51)"}}>{props.props.token1} / {props.props.token2}</Col>
+            <Col span="4" style={{ textAlign: "left", color: "rgb(242,193,51)" }}><Popover content={lpTokenPop} title="Single LP token price" trigger="hover">{props.props.lpNumber} / {props.props.lpTotalValue} $</Popover></Col>
             <Col span="4" style={{ textAlign: "left" }}>From last deposit / withdrawal</Col>
-            <Col span="4" style={{ textAlign: "left" }}>{props.props.ILPercentage}% / {props.props.ILDollar} $</Col>
+            <Col span="4" style={{ textAlign: "left", color: "rgb(242,193,51)" }}>{props.props.ILPercentage}% / {props.props.ILDollar} $</Col>
             <Col span="4" style={{ textAlign: "left" }}>Current fee: {props.props.fee} $</Col>
             <Col span="4" style={{ textAlign: "left" }}>Harvested / Compounded {props.props.harvestCompoundTimes} times</Col>
           </Row>
           <Row>
             <Col span="3" style={{ textAlign: "left" }}> TVL: $ {props.props.tvl} </Col>
             <Col span="4" style={{ textAlign: "left" }}>Distribution:</Col>
-            <Col span="4" style={{ textAlign: "left" }}>{props.props.last}</Col>
+            <Col span="4" style={{ textAlign: "left", color: "rgb(242,193,51)" }}>{props.props.last}</Col>
             <Col span="4" style={{ textAlign: "left" }}>0 if: {props.props.token1}={props.props.zero1} $ and {props.props.token2}={props.props.zero2}$</Col>
             <Col span="4" style={{ textAlign: "left" }}>Optimal compounding time</Col>
             <Col span="4" style={{ textAlign: "left" }}>Average compounding time: {props.props.avgCompound} days</Col>
           </Row>
           <Row>
             <Col span="3" style={{ textAlign: "left" }}>
-              <a href={props.props.liquidityLink}>Add liquidity</a>
+              <a href={props.props.liquidityLink} style={{ color: "#0000EE" }}>Add liquidity</a>
             </Col>
-            <Col span="4" style={{ textAlign: "left" }}>{props.props.distribution1} / {props.props.distribution2}</Col>
+            <Col span="4" style={{ textAlign: "left", color: "rgb(242,193,51)" }}>{props.props.distribution1} / {props.props.distribution2}</Col>
             <Col span="4" style={{ textAlign: "left" }}>{props.props.yieldToken} token / {props.props.yieldDollar} $</Col>
             <Col span="4" style={{ textAlign: "left" }}>IL alert: <Popover content={tgAlertPop} title="Telegram Alert" trigger="click"><BellFilled style={{ cursor: "pointer", fontSize: "1.5em" }} /></Popover></Col>
-            <Col span="4" style={{ textAlign: "left" }}>{props.props.optimal} time a day</Col>
+            <Col span="4" style={{ textAlign: "left", color: "rgb(242,193,51)" }}>{props.props.optimal} time a day</Col>
             <Col span="4" style={{ textAlign: "left" }}>Total paid fees: {props.props.totalFees} $</Col>
           </Row>
         </div>
